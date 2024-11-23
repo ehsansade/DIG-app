24a9a3bb7be6ae50a7b023b3a745af804054b6f1
Ehsan Sadeghi <30415672+ehsansade@users.noreply.github.com>
2024-11-22 17:46:44 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+#from fuzzywuzzy import process  
+import re
+import numpy as np
+from fuzzywuzzy import fuzz
+from difflib import SequenceMatcher
 st.title("NLP-Powered Data Insights Generator")
